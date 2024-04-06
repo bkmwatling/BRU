6907e5fe669088a16b7231d5375a722ad47a7a26
cdltlehf <cdltlehf@node06.toc.lab>
2024-04-06 18:36:44 +0900

    Fix typo

--- benchmark/makefiles/step.mk
+++ benchmark/makefiles/step.mk
@@ -10,6 +10,6 @@ append-all-step: $(STEP_ALL)
 $(STEP_ALL): $(BENCHMARK_ALL_FULL) $(BENCHMARK_ALL_PARTIAL) \
 		| $(STEP_DIR) $(LOGS_DIR) $(VENV)
 	@echo "make $@"
-	@$(PYTHON) src/append_step.py --regex-type all $^ $(STEP_DIR)\
+	@$(PYTHON) src/append_step.py --regex-type all $^ $(STEP_DIR) \
 		2> $(LOGS_DIR)/$(basename $(notdir $@)).log \
 		|| rm -f $(STEP_ALL)
