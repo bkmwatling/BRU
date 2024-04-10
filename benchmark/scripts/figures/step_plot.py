ed245e01b643e8f4d6d5ff5dfd562ba241a0fdf0
cdltlehf <cdltlehf@node06.toc.lab>
2024-04-10 17:36:39 +0900

    Add figures comparing constructions

--- benchmark/scripts/figures/step_plot.py
+++ benchmark/scripts/figures/step_plot.py
@@ -16,6 +16,7 @@
 import sys
 import logging
 import importlib
+import itertools
 from pathlib import Path
 
 import numpy as np
@@ -43,18 +44,22 @@ steps = load_steps_dict(data_dir)
 logging.basicConfig(level=logging.INFO, stream=sys.stderr)
 
 # %%
+import importlib
+importlib.reload(plt)
 matching_type = 'full'
 scheduler = 'spencer'
-memo_scheme = 'in'
+memo_scheme = 'none'
 input_types = ['positive', 'negative']
 xs = np.concatenate([
     steps[matching_type]['thompson'][scheduler][memo_scheme][input_type]
     for input_type in input_types
 ])
+xlabel = "-".join([matching_type, 'thompson', scheduler, memo_scheme])
 ys = np.concatenate([
     steps[matching_type]['flat'][scheduler][memo_scheme][input_type]
     for input_type in input_types
 ])
+ylabel = "-".join([matching_type, 'flat', scheduler, memo_scheme])
 zs = np.concatenate(
     [eliminateds[memo_scheme][input_type] for input_type in input_types])
 threshold = 1
@@ -67,29 +72,70 @@ maximum = np.max(total, where=xs <= quartile_3 + 1.5 * iqr, initial=-np.inf)
 minimum = np.min(total, where=xs >= quartile_1 - 1.5 * iqr, initial=np.inf)
 
 # %%
+# Histogram 2D
+import importlib
+import itertools
+importlib.reload(plt)
+
+options = itertools.product(
+    ['full', 'partial'],
+    [('spencer', 'none'), ('spencer', 'cn'), ('spencer', 'in'), ('lockstep', 'none')]
+)
+threshold = 1
+
+for matching_type, (scheduler, memo_scheme) in options:
+    input_types = ['positive', 'negative']
+    xs = np.concatenate([
+        steps[matching_type]['thompson'][scheduler][memo_scheme][input_type]
+        for input_type in input_types
+    ])
+    xlabel = "-".join([matching_type, 'thompson', scheduler, memo_scheme])
+    ys = np.concatenate([
+        steps[matching_type]['flat'][scheduler][memo_scheme][input_type]
+        for input_type in input_types
+    ])
+    ylabel = "-".join([matching_type, 'flat', scheduler, memo_scheme])
+    zs = np.concatenate(
+        [eliminateds[memo_scheme][input_type] for input_type in input_types])
+    # Range excluding outliers
+    total = xs + ys
+    quartile_1 = np.quantile(total, 0.25)
+    quartile_3 = np.quantile(total, 0.75)
+    iqr = quartile_3 - quartile_1
+    maximum = np.max(total, where=xs <= quartile_3 + 1.5 * iqr, initial=-np.inf)
+    minimum = np.min(total, where=xs >= quartile_1 - 1.5 * iqr, initial=np.inf)
+    x_space = np.geomspace(1, max(xs+ys), 30)
+    y_space = np.geomspace(1, max(ys+ys), 30)
+    
+    fig, axs = plt.subplots(1, 2, figsize=(8, 4))
+    
+    h, _, _, _ = axs[0].hist2d(xs[zs <= threshold], ys[zs <= threshold], bins=(x_space, y_space), vmin=1, norm='log')
+    axs[0].set_xscale('log')
+    axs[0].set_yscale('log')
+    axs[0].set_xlabel(xlabel + f" (elim. <= {threshold})")
+    axs[0].set_ylabel(ylabel)
+    axs[0].axis('square')
+    
+    vmin = np.min(h)
+    
+    axs[1].hist2d(xs[zs > threshold], ys[zs > threshold], bins=(x_space, y_space), norm='log', vmin=1, vmax=vmax)
+    axs[1].set_xscale('log')
+    axs[1].set_yscale('log')
+    axs[1].set_xlabel(xlabel + f" (elim. > {threshold})")
+    axs[1].axis('square')
+    
+    plt.subplots_adjust(wspace=0.3)
+    output_dir = Path('./outputs')
+    output_dir.mkdir(parents=True, exist_ok=True)
+    filename = "-".join([matching_type, scheduler, memo_scheme]) + '.pdf'
+    plt.savefig(output_dir / filename)
+    plt.show()
+
+# %%
 # Scatter plot
 logging.basicConfig(level=logging.INFO, stream=sys.stderr, force=True)
 plt.scatter(xs[zs < threshold], ys[zs < threshold], c='blue')
 plt.scatter(xs[zs >= threshold], ys[zs >= threshold], c='orange')
-plt.xscale('log')
-plt.yscale('log')
-plt.axis('square')
-plt.show()
-
-# %%
-# Histogram 2D
-x_space = np.geomspace(1, max(xs), 100)
-y_space = np.geomspace(1, max(ys), 100)
-h = plt.hist2d(xs[zs < threshold], ys[zs < threshold], bins=(x_space, y_space), norm='log')
-plt.colorbar(h[3])
-plt.xscale('log')
-plt.yscale('log')
-plt.axis('square')
-plt.show()
-
-# Histogram 2Dace), norm='log')
-h = plt.hist2d(xs[zs >= threshold], ys[zs >= threshold], bins=(x_space, y_space), norm='log')
-plt.colorbar(h[3])
 plt.xscale('log')
 plt.yscale('log')
 plt.axis('square')
--- benchmark/scripts/tables/memo_statistics.sh
+++ benchmark/scripts/tables/memo_statistics.sh
@@ -1,5 +1,4 @@
 #!bash
-sed -n -e '1,2p' -e '4,5p' data/analysis/memo_size/all-full-thompson-spencer-none.tex
 (
   for file in data/analysis/memo_size/all-{full,partial}-{thompson,flat}-spencer-{cn,in}.tex; do
     sed -n -e '3p' -e '6p' $file | tr -d '\n' | sed 's/\\\\/\&/'
